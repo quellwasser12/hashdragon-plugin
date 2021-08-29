5c1de82e2d8a3d45cc63e964585a91516702dfef
Sire Quellwasswer <quellwasser@hotmail.com>
2021-08-29 16:47:31 +0100

    more cleanup

--- hashdragon-plugin/breed_dialog.py
+++ hashdragon-plugin/breed_dialog.py
@@ -9,36 +9,38 @@ from .utils import *
 class BreedDialog(BaseEventDialog):
 
     def __init__(self, hashdragon, parent, db):
-        self.db = db
         list_hashdragons = db.list_hashdragons()
         list_hashdragons.remove(hashdragon.hashdragon())
         self.hashdragons = list_hashdragons
-        BaseEventDialog.__init__(self, hashdragon, 'Breed', parent)
+        BaseEventDialog.__init__(self, hashdragon, 'Breed', parent, db)
 
     def build_layout(self, action):
         self.layout = QGridLayout()
         self.layout.setColumnStretch(1, 4)
         self.layout.setColumnStretch(2, 4)
 
-        breedwithlabel = QLabel("Breed with")
+        self.layout.addWidget(QLabel("Breed"), 0, 0)
+        self.layout.addWidget(QLabel(self.hashdragon.hashdragon()), 0, 1)
 
-        self.layout.addWidget(breedwithlabel, 0, 0)
+        breedwithlabel = QLabel("with")
+
+        self.layout.addWidget(breedwithlabel, 1, 0)
 
         self.breed_with_cb = QComboBox()
         self.breed_with_cb.addItems(self.hashdragons)
 
         # self.hibernate_to = PayToEdit(self.main_window)
         breedwithlabel.setBuddy(self.breed_with_cb)
-        self.layout.addWidget(self.breed_with_cb, 0, 1)
+        self.layout.addWidget(self.breed_with_cb, 1, 1)
 
         sendhatchlingtolabel = QLabel("Send Spawn to")
 
-        self.layout.addWidget(sendhatchlingtolabel, 1, 0)
+        self.layout.addWidget(sendhatchlingtolabel, 2, 0)
 
         self.send_hatchling_to = QLineEdit()
         # self.hibernate_to = PayToEdit(self.main_window)
         sendhatchlingtolabel.setBuddy(self.send_hatchling_to)
-        self.layout.addWidget(self.send_hatchling_to, 1, 1)
+        self.layout.addWidget(self.send_hatchling_to, 2, 1)
 
         self.layout.addWidget(self.buttonBox)
         self.setLayout(self.layout)
@@ -54,17 +56,13 @@ class BreedDialog(BaseEventDialog):
         fee = None
 
         # Current location of the hashdragon, i.e. last valid tx for this hashdragon
-        current_txn_ref = self.db.get_hashdragon_by_hash(self.hashdragon.hashdragon()).get_current_tx()
-        ok, r = self.main_window.wallet.network.get_raw_tx_for_txid(current_txn_ref, timeout=10.0)
+        current_txn_ref1 = self.db.get_hashdragon_by_hash(self.hashdragon.hashdragon()).get_current_tx()
+        ok, r = self.main_window.wallet.network.get_raw_tx_for_txid(current_txn_ref1, timeout=10.0)
 
         output_index = -1
 
-
-        tx = find_last_valid_tx_for_hashdragon(self.main_window.wallet, self.hashdragon.hashdragon(), self.main_window.config)
+        current_txn_ref2 = self.db.get_hashdragon_by_hash(self.breed_with_cb.currentText()).get_current_tx()
 
         print("Current hashdragon: %s" % self.hashdragon.hashdragon())
-        print("Current txnref: %s" % current_txn_ref)
-        if tx is not None:
-            print("Found txnref: %s" % tx.txid())
-        else:
-            print("No txnref found.")
+        print("Current txnref1: %s" % current_txn_ref1)
+        print("Current txnref2: %s" % current_txn_ref2)
